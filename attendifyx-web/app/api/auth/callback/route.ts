0075d15471f4fcd5cb49b9bfc97baf6363e0e243
Josuan Leonardo Hulom <110836440+hulomjosuan21@users.noreply.github.com>
2026-01-11 12:42:15 +0800

    refactor(api): change base url to api url from env

--- attendifyx-web/app/api/auth/callback/route.ts
+++ attendifyx-web/app/api/auth/callback/route.ts
@@ -1,18 +1,21 @@
-import {createServerSupabaseClient} from "@/lib/supabase/server";
-import {NextResponse} from "next/server";
+import { createServerSupabaseClient } from "@/lib/supabase/server";
+import { NextResponse } from "next/server";
 
 export async function GET(request: Request) {
-    const {searchParams, origin} = new URL(request.url);
+    const { searchParams } = new URL(request.url);
     const code = searchParams.get("code");
-    const next = searchParams.get("next") ?? "/event";
+    const next = searchParams.get("next") ?? "/events";
+
+    const baseUrl = process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000";
 
     if (code) {
         const supabase = await createServerSupabaseClient();
-        const {error} = await supabase.auth.exchangeCodeForSession(code);
+        const { error } = await supabase.auth.exchangeCodeForSession(code);
 
         if (!error) {
-            return NextResponse.redirect(`${origin}${next}`);
+            return NextResponse.redirect(`${baseUrl}${next}`);
         }
     }
-    return NextResponse.redirect(`${origin}/auth/auth-code-error`);
+
+    return NextResponse.redirect(`${baseUrl}/auth/auth-code-error`);
 }
