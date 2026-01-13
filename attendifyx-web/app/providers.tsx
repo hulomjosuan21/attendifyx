8e9dc86d0362339c1c7a8180f3fc8e3fabf36cd6
Josuan Leonardo Hulom <110836440+hulomjosuan21@users.noreply.github.com>
2026-01-13 08:37:15 +0800

    style: format code for consistency in providers.tsx

--- attendifyx-web/app/providers.tsx
+++ attendifyx-web/app/providers.tsx
@@ -1,26 +1,24 @@
-'use client'
-import {ReactNode} from "react";
-import {
-    QueryClientProvider,
-} from '@tanstack/react-query'
+"use client";
+import { ReactNode } from "react";
+import { QueryClientProvider } from "@tanstack/react-query";
 import queryClient from "@/lib/query-client";
-import {ReactQueryDevtools} from '@tanstack/react-query-devtools'
-import {Toaster} from "@/components/ui/sonner"
-import {ThemeProvider} from "@/app/styles/theme/theme-provider";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { Toaster } from "@/components/ui/sonner";
+import { ThemeProvider } from "@/app/styles/theme/theme-provider";
 
-export default function Providers({children}: { children: ReactNode }) {
-    return (
-        <QueryClientProvider client={queryClient}>
-            <ThemeProvider
-                attribute="class"
-                defaultTheme="system"
-                enableSystem
-                disableTransitionOnChange
-            >
-                {children}
-                <Toaster/>
-                <ReactQueryDevtools initialIsOpen={false}/>
-            </ThemeProvider>
-        </QueryClientProvider>
-    )
+export default function Providers({ children }: { children: ReactNode }) {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider
+        attribute="class"
+        defaultTheme="system"
+        enableSystem
+        disableTransitionOnChange
+      >
+        {children}
+        <Toaster />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </ThemeProvider>
+    </QueryClientProvider>
+  );
 }
